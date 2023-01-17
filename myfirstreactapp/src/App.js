2b97b29e254c969786048fe20f1c47fb461c10cb
Seun1601 <114428457+Seun1601@users.noreply.github.com>
2023-01-17 20:47:02 +0100

    deleted some files

--- myfirstreactapp/src/App.js
+++ myfirstreactapp/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+  import './App.css';
+import Godabeg from './Godabeg';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let name = "Seun";
+  const age = 21;
+
+
+  const [userName, setUserName] = useState('Suya')
+function changeName (){
+  setUserName('Pizza is neat')
+}
+//e.PreventDefault(); will stop your page from reloading when a form is submitted
+const peeps = ['Halima', 'Seun', 'Raddie', 'Gabe', 'Halima', 'Ola', 'Demilade','Temi']
+
+  return(
+    <div>
+      <h1 className='something'>Hello {name}</h1>
+      {age >= 18 ? <p>You are a big girl oo</p> : <p>You are a small girl </p>}
+      <p>Hi {name} you are {age} years old</p>
+      <p>{userName}</p>
+      <button onClick={changeName}>Click me</button>
+      <Godabeg un={userName}/>
+
+      {peeps.map((cur, index) => <h1 key={index}>{cur}</h1>)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
+  
   );
 }
 
 export default App;
+// State: Javascript object that tells your js app what to look like at a time
+
+// hooks: Use state(To create a state), Use Context, Use Effect(To fetch data from apis), Use Ref, Use Memo
---
+++ myfirstreactapp/src/Godabeg.js
@@ -0,0 +1,109 @@
+import React from 'react'
+
+const Godabeg = ({un}) => {
+  return (
+    <div>
+        <p>What starts with an E, ends with an E, but only contains one letter?
+Envelope
+
+What has a heart that doesn’t beat?
+An artichoke
+
+What begins with T, ends with T, and has T in it?
+A teapot
+
+What has four legs in the morning, two legs in the afternoon, and three legs in the evening?
+A human (as a baby, an adult, and an elderly person with a cane)
+
+What kind of room has no doors or windows?
+A mushroom
+
+What has a head, a tail, but no body?
+A coin
+
+What has a face but no eyes, a hand but no arms, and feet but no legs?
+A clock
+
+What can you catch but not throw?
+A cold
+
+What is so fragile that saying its name breaks it?
+Silence
+
+
+I am taken from a mine, and shut up in a wooden case, from which I am never released, and yet I am used by almost every person. What am I?
+A pencil lead
+
+What has keys but can't open locks?
+A keyboard
+
+I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?
+A fire
+
+What has a tail but no body?
+A coin
+
+I am not alive but I grow, I don't have lungs but I need air, I don't have a mouth but water kills me. What am I?
+A fire
+
+What has a tongue but cannot talk?
+A shoe
+
+What is so light it can be carried by the wind?
+A feather
+
+What starts with an E, ends with an E, but only contains one letter?
+Envelope
+
+What is full of holes but still holds water?
+A sponge
+
+What goes up but never comes down?
+Age
+
+What is always in front of you but can't be seen?
+The future
+
+What is always in the past, but never in the future?
+History
+
+What is so fragile that saying its name breaks it?
+Silence
+
+What can be cracked, made, told, and played?
+A joke
+
+What is so small that you can't even see it?
+An atom
+
+What is so large that you can't even see it?
+The universe
+
+What is at the end of a rainbow?
+W
+
+What has a neck but no head, a body but no arms, and legs but no feet?
+A bottle
+
+What is always in the present but never in the past or future?
+The present moment
+
+What can you see through but not in?
+A window
+
+What can you break without touching it?
+A heart
+
+I am not alive but I grow, I don't have lungs but I need air, I don't have a mouth but water kills me. What am I?
+Fire
+
+What has four legs in the morning, two legs in the afternoon, and three legs in the evening?
+A human as a baby, an adult, and an elderly person with a cane
+
+</p>
+<p>{un}</p>
+    </div>
+  );
+}
+
+export default Godabeg
